64b29ad5b3a244feb699b37d74cd6c346b261951
meke202122 <93667019+meke202122@users.noreply.github.com>
2023-03-18 08:07:28 -0700

    Delete contracts directory

--- contracts/interface/IPriceFeeder.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0
-
-pragma solidity 0.7.6;
-
-
-interface IPriceFeeder {
-    function price() external view returns (uint256 lastPrice, uint256 lastTimestamp);
-}
