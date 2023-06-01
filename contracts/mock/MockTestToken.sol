6ab8f5b9f7144540e8c691f87ec8756a41aa08ce
meke202122 <meke2088@gmail.com>
2023-06-01 19:12:28 +0800

    rename testtoken.sol to MockTestToken.sol

--- contracts/mock/MockTestToken.sol
+++ contracts/mock/MockTestToken.sol
@@ -4,7 +4,7 @@ pragma solidity 0.7.6;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract TestToken is ERC20 {
+contract MockTestToken is ERC20 {
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {
         _mint(msg.sender, 1e19);
     }
